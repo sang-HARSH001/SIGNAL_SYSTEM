759c99feada095bcdea067ca4d62cc2cbbe25df8
sang-HARSH001 <killerfighter001@gmail.com>
2025-09-18 00:16:38 +0530

    Update sample_signals.py

--- sample_signals.py
+++ sample_signals.py
@@ -2,12 +2,13 @@ import numpy as np
 
 def get_sample_signals():
     return {
-        'Exponential Decay (Energy)': np.array([np.exp(-n) for n in range(50)]),
-        'Sine Wave (Power, Periodic)': np.sin(np.linspace(0, 20 * np.pi, 100)),
-        'Cosine Wave (Power, Periodic)': np.cos(np.linspace(0, 20 * np.pi, 100)),
-        'Unit Step Signal (Causal)': np.array([1 for _ in range(50)]),
+        'Exponential Decay ': np.array([np.exp(-n) for n in range(50)]),
+        'Sine Wave ': np.sin(np.linspace(0, 20 * np.pi, 100)),
+        'Cosine Wave ': np.cos(np.linspace(0, 20 * np.pi, 100)),
+        'Unit Step Signal ': np.array([1 for _ in range(50)]),
         'Unit Impulse Signal': np.array([1] + [0]*49),
-        'Ramp Signal (Causal)': np.array([n for n in range(50)]),
+        'Ramp Signal': np.array([n for n in range(50)]),
 
     }
+
 
